144aec8266985d9a71793288a680dd367156d36b
Ashish Sharma <ashishsharma01710171@gmail.com>
2025-10-24 23:26:15 +0530

    Update constants.js

--- services/constants.js
+++ services/constants.js
@@ -42,7 +42,7 @@ export const ID_UNIVERSAL_VIDEO_1 = '18iUFO_VcL_7dyPpGYz3Tp0F0YClS4JtC';
 export const ID_UNIVERSAL_VIDEO_2 = '1LQKHo3T6yEwSiqRSAxFvtHQTJu6Ptv_O';
 
 // NEW: Department-Specific Image IDs/URLs
-export const ID_IMAGE_HOSTEL = '1teLNDg1xhI__VX014HGV84u7v1MdsSIr';
+export const ID_IMAGE_HOSTEL = '1L2hCkm-X9FHcn8YBv6weFzGRulMEjAbu';
 export const URL_IMAGE_HOSTEL = `${GOOGLE_DRIVE_BASE_URL}${ID_IMAGE_HOSTEL}`;
 
 
